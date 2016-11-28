cba210d89143ac0a2831806dd79e3bd4c66b0c14
danielzengqx <cheer_zeng@163.com>
2016-11-28 22:50:52 +0800

    update template files

--- teacher_info/views.py
+++ teacher_info/views.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+# coding=gbk
+from __future__ import division
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from rating.models import Teacher
 # Create your views here.
 from django.contrib.auth.decorators import login_required
+
 
 def teacher_info(request):
 	teachers =  Teacher.objects.all()
@@ -33,12 +37,13 @@ def get_ref_id():
 	except:
 		return ref_id
 
-
 def teacher_detail(request, tid):
-	teachers =  Teacher.objects.all()
-	for t in teachers:
-		print "here is teacher %s, name %s" %(t.tid, t.name)
+	try:
+		teacher =  Teacher.objects.get(tid=tid)
+	except:
+		HttpResponse("不存在该资源")
 
+	print "here is teacher %s, name %s" %(teacher.score1_content, teacher.name)
 	# template = "teacher_info.html"
 	# template = "teacher_widget.html"
 	template = "profile.html"
@@ -47,7 +52,16 @@ def teacher_detail(request, tid):
 
 
 	context = {
-				"teachers": teachers
+				"name": teacher.name,
+				"major": teacher.major,
+				"score1": format(teacher.score1/10,'.2%'),
+				"score1_content": teacher.score1_content,
+				"score2":  format(teacher.score2/10,'.2%'),
+				"score2_content": teacher.score2_content,
+				"score3":  format(teacher.score3/10,'.2%'),
+				"score3_content": teacher.score3_content,
+				"intro": teacher.intro,
+
 				}
 
 
